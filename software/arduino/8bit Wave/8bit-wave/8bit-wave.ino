77315017f25eeefc8ac1f69914d52e212f01f323
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2020-02-23 00:35:23 +0100

    Hello blink LED

---
+++ software/arduino/8bit Wave/8bit-wave/8bit-wave.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "constants.h"
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
